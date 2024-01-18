64fb8d7b110697f4d0b0ffb473ae7b94bcb0f83b
Dharmendra Kumar Prasad <33777797+Dharmendra-prasad@users.noreply.github.com>
2024-01-18 07:55:05 +0000

    datatype and variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "dprasad@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+/*
+Avoid using var variable because it is not understand the scope in Javascript
+instead use let variable.
+*/
+
+//accountId = 2
+accountEmail = "dp@gmail.com"
+accountPassword = "3242"
+accountCity = "Indore"
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+//number => 2^53
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
