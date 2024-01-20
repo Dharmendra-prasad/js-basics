eaa9406a247cc69c5deb13d6ea6bf5e1e4a3f794
Dharmendra Kumar Prasad <33777797+Dharmendra-prasad@users.noreply.github.com>
2024-01-20 06:40:14 +0000

    compairision

---
+++ 01_basics/04_Compairision.js
@@ -0,0 +1,16 @@
+console.log("2">1); //true
+
+console.log(null>0);//False
+console.log(null == 0); //false
+console.log(null>=0); //true     
+//null is converted as 0 in case of >< >= <= but not in case of ==
+
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+console.log(undefined == 0); //fasle
+
+
+// === strict check
+console.log("2" === 2); //false
+
+
