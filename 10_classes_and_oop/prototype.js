e75aea6d366e9200330d0ab03e0ca3e0cb29c0a4
Dharmendra Kumar Prasad <33777797+Dharmendra-prasad@users.noreply.github.com>
2024-01-31 09:14:09 +0000

    inheritance

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,68 @@
+// let myName = "hitesh     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.hitesh = function(){
+    console.log(`hitesh is present in all objects`);  // creating prototype manually on top of object or in object
+}
+
+Array.prototype.heyHitesh = function(){
+    console.log(`Hitesh says hello`);
+}
+
+// heroPower.hitesh()   //here we can access prototype hitesh through object heroPower
+// myHeros.hitesh()     //here accessing prototype hitesh through array myHeros
+// myHeros.heyHitesh()  //same here
+// heroPower.heyHitesh() //same here
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
